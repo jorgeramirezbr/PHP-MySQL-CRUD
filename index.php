dc3db878f0c6977a68a79c7132b45ce7b364e231
jorge <jorge.ramirez.br@gmail.com>
2023-09-11 01:03:17 +0200

    Save Task

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include('db.php');
+include('includes/header.php');
+?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+
+            <?php if(isset($_SESSION['message'])) { ?>
+                <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+                <?= $_SESSION['message'] ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php session_unset(); } ?>
+
+            <div class="card card-body">
+                <form action="save_task.php" method="POST">
+                    <div class="form-group mb-3">
+                        <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
+                    </div>
+                    <div class="form-group mb-3">
+                        <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
+                    </div>
+                    <div class="form-group text-center">
+                        <input type="submit" class="btn btn-success" name="save_task" value="Save Task">
+                    </div>                    
+                </form>
+            </div>
+
+        </div>
+        <div class="col-md-8">
+
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
